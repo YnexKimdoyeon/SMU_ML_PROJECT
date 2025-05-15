5f5a9259f16c371e5048811a7b87ca7b978c811b
Doyeon Kim <kims2369@naver.com>
2025-05-15 16:50:22 +0900

    크롤링 코드 개선 selenium -> bs4로

--- 약품/약학정보 크롤링.py
+++ 약품/약학정보 크롤링.py
@@ -3,6 +3,7 @@ import time
 from openpyxl.workbook import Workbook
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome()
 driver.get("https://www.health.kr/interaction/drug.asp")
@@ -22,20 +23,19 @@ for Number,j in enumerate(alist):
         break
 
 input()
-rows = driver.find_elements(By.CSS_SELECTOR, "table#result_drug tr")
+print("추출시작")
 
-# 3. 워크북 생성
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
+table = soup.select_one("table#result_drug")
+
 wb = Workbook()
 ws = wb.active
 ws.title = "결과 테이블"
 
-# 4. tr > td 값 추출해서 엑셀에 저장
-for row in rows:
-    cells = row.find_elements(By.TAG_NAME, "th")  # 제목 셀
-    if not cells:
-        cells = row.find_elements(By.TAG_NAME, "td")  # 일반 셀
-    row_data = [cell.text.strip() for cell in cells]
+for tr in table.select("tr"):
+    cells = tr.find_all(["th", "td"])
+    row_data = [cell.get_text(strip=True) for cell in cells]
     ws.append(row_data)
 
-# 5. 엑셀 저장
 wb.save("result_drug_table.xlsx")
